5efa91056a2dc4647ce0f4d37c0a6966651dd001
amandabrousson <amanda.brousson@gmail.com>
2023-09-28 11:12:41 -0300

    sept

--- JSS/main.js
+++ JSS/main.js
@@ -17,3 +17,66 @@ window.onscroll = () => {
     });
 };
 
+/* FUNCIONES DE SERVICIOS */
+
+const texto1 = document.querySelector('#texto1');
+const cierretexto1 = document.querySelector('#cierretexto1');
+const popup1 = document.querySelector('#popup1');
+
+texto1.addEventListener("click", ()=>{popup1.showModal();})
+cierretexto1.addEventListener("click", ()=>{popup1.close();})
+
+const texto2 = document.querySelector('#texto2');
+const cierretexto2 = document.querySelector('#cierretexto2');
+const popup2 = document.querySelector('#popup2');
+
+texto2.addEventListener("click", ()=>{popup2.showModal();})
+cierretexto2.addEventListener("click", ()=>{popup2.close();})
+
+const texto3 = document.querySelector('#texto3');
+const cierretexto3 = document.querySelector('#cierretexto3');
+const popup3 = document.querySelector('#popup3');
+
+texto3.addEventListener("click", ()=>{popup3.showModal();})
+cierretexto3.addEventListener("click", ()=>{popup3.close();})
+
+const texto4 = document.querySelector('#texto4');
+const cierretexto4 = document.querySelector('#cierretexto4');
+const popup4 = document.querySelector('#popup4');
+
+texto4.addEventListener("click", ()=>{popup4.showModal();})
+cierretexto4.addEventListener("click", ()=>{popup4.close();})
+
+/* FUNCIONES DE STAFF */
+
+// const hidesb = document.getElementById('hidesb');
+// const texthide = document.getElementById('texthide');
+
+// hidesb.addEventListener('click', toggleText);
+
+// function toggleText(){
+//     texthide.classList.toggle('show');
+
+//     if(texthide.classList.contains('show')){
+//         hidesb.innerHTML = 'Ver menos';
+//     }
+//         else{
+//             hidesb.innerHTML = 'Ver más';
+//         }
+// }
+
+const showsb = document.getElementsByClassName('show');
+showsb.addEventListener('click', ()=>{
+    let div = document.createElement('div');
+    div.innerHTML = ` 
+    
+        <img src="img/871577_6157dacac0794941857a999841b208cc~mv2 (1).jpg">  </img>
+    
+    
+    `;
+    
+
+    document.body.section.append();
+})
+
+
